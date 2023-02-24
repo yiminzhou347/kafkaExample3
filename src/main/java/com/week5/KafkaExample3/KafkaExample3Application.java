1de86a62d0fed77024b08826cfbd53dc79d76c99
yiminzhou347 <95442563+yiminzhou347@users.noreply.github.com>
2023-02-23 23:32:13 -0500

    first commit

---
+++ src/main/java/com/week5/KafkaExample3/KafkaExample3Application.java
@@ -0,0 +1,13 @@
+package com.week5.KafkaExample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExample3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/week5/KafkaExample3/KafkaExample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week5.KafkaExample3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
